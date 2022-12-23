2370325dfcf5db499f08b83a718ae6caa0bcbaae
ZeppLu <luzepu678@gmail.com>
2022-12-24 00:34:25 +0800

    (TEMP) set ninja in verbose mode

--- scripts/appveyor-win.sh
+++ scripts/appveyor-win.sh
@@ -23,8 +23,8 @@ cmake \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX="$BUILD_ROOT/opus-prefix" \
 	..
-ninja
-ninja install
+ninja -v
+ninja install -v
 cd ../..
 
 wget https://mirror.firedaemon.com/OpenSSL/openssl-1.1.1s.zip && 7z x openssl-1.1.1s.zip

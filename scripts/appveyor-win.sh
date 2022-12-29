a8daed646a81172b17db6f0f6798a961b432a79d
ZeppLu <luzepu678@gmail.com>
2022-12-29 14:01:31 +0800

    use vcpkg, sencond try

--- scripts/appveyor-win.sh
+++ scripts/appveyor-win.sh
@@ -7,6 +7,8 @@ BUILD_ROOT="$(echo $BUILD_ROOT | sed 's|^/\([a-z]\)|\1:|g')" # replace /c/... by
 echo "BUILD_ROOT=$BUILD_ROOT"
 
 vcpkg install --triplet x64-windows yasm opus sdl2 protobuf
+VCPKG_ROOT="tools/vcpkg/installed/x64-windows"
+export PATH="/c/$VCPKG_ROOT/tools/yasm:/c/$VCPKG_ROOT/tools/protobuf:$PATH"
 
 wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.1-latest-win64-gpl-shared-5.1.zip && 7z x ffmpeg-n5.1-latest-win64-gpl-shared-5.1.zip
 mv ffmpeg-n5.1-latest-win64-gpl-shared-5.1 ffmpeg-prefix
@@ -14,11 +16,11 @@ mv ffmpeg-n5.1-latest-win64-gpl-shared-5.1 ffmpeg-prefix
 wget https://mirror.firedaemon.com/OpenSSL/openssl-1.1.1s.zip && 7z x openssl-1.1.1s.zip
 
 PYTHON="C:/Python37/python.exe"
-"$PYTHON" -m pip install protobuf==3
+"$PYTHON" -m pip install "protobuf>=3"
 
 QT_PATH="C:/Qt/5.15/msvc2019_64"
 
-COPY_DLLS="$PWD/openssl-1.1/x64/bin/libcrypto-1_1-x64.dll $PWD/openssl-1.1/x64/bin/libssl-1_1-x64.dll /c/tools/vcpkg/installed/x64-windows/bin/SDL2.dll $PWD/ffmpeg-prefix/bin/avcodec-59.dll $PWD/ffmpeg-prefix/bin/avutil-57.dll $PWD/ffmpeg-prefix/bin/swresample-4.dll "
+COPY_DLLS="$PWD/openssl-1.1/x64/bin/libcrypto-1_1-x64.dll $PWD/openssl-1.1/x64/bin/libssl-1_1-x64.dll /c/$VCPKG_ROOT/bin/SDL2.dll $PWD/ffmpeg-prefix/bin/avcodec-59.dll $PWD/ffmpeg-prefix/bin/avutil-57.dll $PWD/ffmpeg-prefix/bin/swresample-4.dll "
 
 echo "-- Configure"
 
@@ -29,8 +31,7 @@ cmake \
 	-DCMAKE_C_COMPILER=cl \
 	-DCMAKE_C_FLAGS="-we4013" \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
-	-DCMAKE_PREFIX_PATH="$BUILD_ROOT/ffmpeg-prefix;$BUILD_ROOT/openssl-1.1/x64;$QT_PATH" \
-	-DCMAKE_TOOLCHAIN_FILE="c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+	-DCMAKE_PREFIX_PATH="C:/$VCPKG_ROOT;$BUILD_ROOT/ffmpeg-prefix;$BUILD_ROOT/openssl-1.1/x64;$QT_PATH" \
 	-DPYTHON_EXECUTABLE="$PYTHON" \
 	-DCHIAKI_ENABLE_TESTS=ON \
 	-DCHIAKI_ENABLE_CLI=OFF \
@@ -57,4 +58,5 @@ mkdir Chiaki && cp build/gui/chiaki.exe Chiaki
 mkdir Chiaki-PDB && cp build/gui/chiaki.pdb Chiaki-PDB
 
 "$QT_PATH/bin/windeployqt.exe" Chiaki/chiaki.exe
+ls -lha Chiaki
 cp -v $COPY_DLLS Chiaki

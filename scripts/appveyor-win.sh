10d56f072036a2a0b5c5da92b836995a02f8e178
ZeppLu <luzepu678@gmail.com>
2022-12-30 12:50:11 +0800

    build ffmpeg with dxva2, 1st try

--- scripts/appveyor-win.sh
+++ scripts/appveyor-win.sh
@@ -2,13 +2,20 @@
 
 set -xe
 
+# temp test
+scripts/build-ffmpeg.sh . \
+	--target-os=win64 --arch=x86_64 --toolchain=msvc \
+	--enable-dxva2 --enable-hwaccel=h264_dxva2 --enable-hwaccel=hevc_dxva2 \
+	--enable-d3d11va --enable-hwaccel=h264_d3d11va --enable-hwaccel=hevc_d3d11va
+FFMPEG_ROOT="$(cygpath -m "$(realpath ffmpeg-prefix)")"  # `cygpath -m` converts path to `C:/...`
+
 vcpkg install --triplet x64-windows yasm opus sdl2 protobuf
 VCPKG_ROOT="C:/tools/vcpkg/installed/x64-windows"
 export PATH="$(cygpath $VCPKG_ROOT)/tools/yasm:$(cygpath $VCPKG_ROOT)/tools/protobuf:$PATH"
 
-wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.1-latest-win64-gpl-shared-5.1.zip && 7z x ffmpeg-n5.1-latest-win64-gpl-shared-5.1.zip
-mv ffmpeg-n5.1-latest-win64-gpl-shared-5.1 ffmpeg-prefix
-FFMPEG_ROOT="$(cygpath -m "$(realpath ffmpeg-prefix)")"  # `cygpath -m` converts path to `C:/...`
+#wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.1-latest-win64-gpl-shared-5.1.zip && 7z x ffmpeg-n5.1-latest-win64-gpl-shared-5.1.zip
+#mv ffmpeg-n5.1-latest-win64-gpl-shared-5.1 ffmpeg-prefix
+#FFMPEG_ROOT="$(cygpath -m "$(realpath ffmpeg-prefix)")"  # `cygpath -m` converts path to `C:/...`
 
 wget https://mirror.firedaemon.com/OpenSSL/openssl-1.1.1s.zip && 7z x openssl-1.1.1s.zip
 OPENSSL_ROOT="$(cygpath -m "$(realpath openssl-1.1/x64)")"
